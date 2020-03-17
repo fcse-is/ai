091f5cb4ad44445e9cad8188709e3aa760fc0f74
Martina Toshevska <martina.tosevska.95@gmail.com>
2020-03-17 18:55:21 +0100

    Additional exercises for uninformed search

---
+++ av3/molecule.py
@@ -0,0 +1,149 @@
+from utils import Problem
+from uninformed_search import *
+
+
+def move_right(x1, y1, x2, y2, x3, y3, obstacles):
+    while x1 < 8 and [x1 + 1, y1] not in obstacles and [x1 + 1, y1] != [x2, y2] and [x1 + 1, y1] != [x3, y3]:
+        x1 += 1
+    return x1
+
+
+def move_left(x1, y1, x2, y2, x3, y3, obstacles):
+    while x1 > 0 and [x1 - 1, y1] not in obstacles and [x1 - 1, y1] != [x2, y2] and [x1 - 1, y1] != [x3, y3]:
+        x1 -= 1
+    return x1
+
+
+def move_up(x1, y1, x2, y2, x3, y3, obstacles):
+    while y1 < 6 and [x1, y1 + 1] not in obstacles and [x1, y1 + 1] != [x2, y2] and [x1, y1 + 1] != [x3, y3]:
+        y1 += 1
+    return y1
+
+
+def move_down(x1, y1, x2, y2, x3, y3, obstacles):
+    while y1 > 0 and [x1, y1 - 1] not in obstacles and [x1, y1 - 1] != [x2, y2] and [x1, y1 - 1] != [x3, y3]:
+        y1 -= 1
+    return y1
+
+
+class Molecule(Problem):
+    def __init__(self, obstacles, initial, goal=None):
+        super().__init__(initial, goal)
+        self.obstacles = obstacles
+
+    def successor(self, state):
+        """За дадена состојба, врати речник од парови {акција : состојба}
+        достапни од оваа состојба. Ако има многу следбеници, употребете
+        итератор кој би ги генерирал следбениците еден по еден, наместо да
+        ги генерирате сите одеднаш.
+
+        :param state: дадена состојба
+        :return:  речник од парови {акција : состојба} достапни од оваа
+                  состојба
+        :rtype: dict
+        """
+        successors = dict()
+
+        h1_x, h1_y = state[0], state[1]
+        o_x, o_y = state[2], state[3]
+        h2_x, h2_y = state[4], state[5]
+
+        # H1
+        x_new = move_right(h1_x, h1_y, o_x, o_y, h2_x, h2_y, self.obstacles)
+        if x_new != h1_x:
+            successors['RightH1'] = (x_new, h1_y, o_x, o_y, h2_x, h2_y)
+
+        x_new = move_left(h1_x, h1_y, o_x, o_y, h2_x, h2_y, self.obstacles)
+        if x_new != h1_x:
+            successors['LeftH1'] = (x_new, h1_y, o_x, o_y, h2_x, h2_y)
+
+        y_new = move_up(h1_x, h1_y, o_x, o_y, h2_x, h2_y, self.obstacles)
+        if y_new != h1_y:
+            successors['UpH1'] = (h1_x, y_new, o_x, o_y, h2_x, h2_y)
+
+        y_new = move_down(h1_x, h1_y, o_x, o_y, h2_x, h2_y, self.obstacles)
+        if y_new != h1_y:
+            successors['DownH1'] = (h1_x, y_new, o_x, o_y, h2_x, h2_y)
+
+        # H2
+        x_new = move_right(h2_x, h2_y, o_x, o_y, h1_x, h1_y, self.obstacles)
+        if x_new != h2_x:
+            successors['RightH2'] = (h1_x, h1_y, o_x, o_y, x_new, h2_y)
+
+        x_new = move_left(h2_x, h2_y, o_x, o_y, h1_x, h1_y, self.obstacles)
+        if x_new != h2_x:
+            successors['LeftH2'] = (h1_x, h1_y, o_x, o_y, x_new, h2_y)
+
+        y_new = move_up(h2_x, h2_y, o_x, o_y, h1_x, h1_y, self.obstacles)
+        if y_new != h2_y:
+            successors['UpH2'] = (h1_x, h1_y, o_x, o_y, h2_x, y_new)
+
+        y_new = move_down(h2_x, h2_y, o_x, o_y, h1_x, h1_y, self.obstacles)
+        if y_new != h2_y:
+            successors['DownH2'] = (h1_x, h1_y, o_x, o_y, h2_x, y_new)
+
+        # O
+        x_new = move_right(o_x, o_y, h1_x, h1_y, h2_x, h2_y, self.obstacles)
+        if x_new != o_x:
+            successors['RightO'] = (h1_x, h1_y, x_new, o_y, h2_x, h2_y)
+
+        x_new = move_left(o_x, o_y, h1_x, h1_y, h2_x, h2_y, self.obstacles)
+        if x_new != o_x:
+            successors['LeftO'] = (h1_x, h1_y, x_new, o_y, h2_x, h2_y)
+
+        y_new = move_up(o_x, o_y, h1_x, h1_y, h2_x, h2_y, self.obstacles)
+        if y_new != o_y:
+            successors['UpO'] = (h1_x, h1_y, o_x, y_new, h2_x, h2_y)
+
+        y_new = move_down(o_x, o_y, h1_x, h1_y, h2_x, h2_y, self.obstacles)
+        if y_new != o_y:
+            successors['DownO'] = (h1_x, h1_y, o_x, y_new, h2_x, h2_y)
+
+        return successors
+
+    def actions(self, state):
+        """За дадена состојба state, врати листа од сите акции што може да
+        се применат над таа состојба
+
+        :param state: дадена состојба
+        :return: листа на акции
+        :rtype: list
+        """
+        return self.successor(state).keys()
+
+    def result(self, state, action):
+        """За дадена состојба state и акција action, врати ја состојбата
+        што се добива со примена на акцијата над состојбата
+
+        :param state: дадена состојба
+        :param action: дадена акција
+        :return: резултантна состојба
+        """
+        return self.successor(state)[action]
+
+    def goal_test(self, state):
+        """Врати True ако state е целна состојба. Даденава имплементација
+        на методот директно ја споредува state со self.goal, како што е
+        специфицирана во конструкторот. Имплементирајте го овој метод ако
+        проверката со една целна состојба self.goal не е доволна.
+
+        :param state: дадена состојба
+        :return: дали дадената состојба е целна состојба
+        :rtype: bool
+        """
+        # H1 O H2
+        return state[1] == state[3] == state[5] and state[0] + 1 == state[2] and state[2] + 1 == state[4]
+
+
+if __name__ == '__main__':
+    obstacles_list = [[0, 1], [1, 1], [1, 3], [2, 5], [3, 1], [3, 6], [4, 2],
+                      [5, 6], [6, 1], [6, 2], [6, 3], [7, 3], [7, 6], [8, 5]]
+    h1_pos = [2, 1]
+    h2_pos = [2, 6]
+    o_pos = [7, 2]
+
+    molecule = Molecule(obstacles_list, (h1_pos[0], h1_pos[1], o_pos[0], o_pos[1], h2_pos[0], h2_pos[1]))
+
+    result = breadth_first_graph_search(molecule)
+    print(result.solution())
+
---
+++ av3/stars.py
@@ -0,0 +1,219 @@
+from utils import Problem
+from uninformed_search import *
+
+
+def k1(x, y, b_x, b_y):  # up up left
+    if 0 < x - 1 < 8 and 0 < y + 2 < 8 and [x - 1, y + 2] != [b_x, b_y]:
+        x -= 1
+        y += 2
+    return x, y
+
+
+def k2(x, y, b_x, b_y):  # up up right
+    if 0 < x + 1 < 8 and 0 < y + 2 < 8 and [x + 1, y + 2] != [b_x, b_y]:
+        x += 1
+        y += 2
+    return x, y
+
+
+def k3(x, y, b_x, b_y):  # right right up
+    if 0 < x + 2 < 8 and 0 < y + 1 < 8 and [x + 2, y + 1] != [b_x, b_y]:
+        x += 2
+        y += 1
+    return x, y
+
+
+def k4(x, y, b_x, b_y):  # right right down
+    if 0 < x + 2 < 8 and 0 < y - 1 < 8 and [x + 2, y - 1] != [b_x, b_y]:
+        x += 2
+        y -= 1
+    return x, y
+
+
+def k5(x, y, b_x, b_y):  # down down right
+    if 0 < x + 1 < 8 and 0 < y - 2 < 8 and [x + 1, y - 2] != [b_x, b_y]:
+        x += 1
+        y -= 2
+    return x, y
+
+
+def k6(x, y, b_x, b_y):  # down down left
+    if 0 < x - 1 < 8 and 0 < y - 2 < 8 and [x - 1, y - 2] != [b_x, b_y]:
+        x -= 1
+        y -= 2
+    return x, y
+
+
+def k7(x, y, b_x, b_y):  # left left down
+    if 0 < x - 2 < 8 and 0 < y - 1 < 8 and [x - 2, y - 1] != [b_x, b_y]:
+        x -= 2
+        y -= 1
+    return x, y
+
+
+def k8(x, y, b_x, b_y):  # left left up
+    if 0 < x - 2 < 8 and 0 < y + 1 < 8 and [x - 2, y + 1] != [b_x, b_y]:
+        x -= 2
+        y += 1
+    return x, y
+
+
+def b1(x, y, k_x, k_y):  # up left
+    if 0 < x - 1 < 8 and 0 < y + 1 < 8 and [x - 1, y + 1] != [k_x, k_y]:
+        x -= 1
+        y += 1
+    return x, y
+
+
+def b2(x, y, k_x, k_y):  # up right
+    if 0 < x + 1 < 8 and 0 < y + 1 < 8 and [x + 1, y + 1] != [k_x, k_y]:
+        x += 1
+        y += 1
+    return x, y
+
+
+def b3(x, y, k_x, k_y):  # down left
+    if 0 < x - 1 < 8 and 0 < y - 1 < 8 and [x - 1, y - 1] != [k_x, k_y]:
+        x -= 1
+        y -= 1
+    return x, y
+
+
+def b4(x, y, k_x, k_y):  # down right
+    if 0 < x + 1 < 8 and 0 < y - 1 < 8 and [x + 1, y - 1] != [k_x, k_y]:
+        x += 1
+        y -= 1
+    return x, y
+
+
+class Stars(Problem):
+    def __init__(self, initial, goal=None):
+        super().__init__(initial, goal)
+
+    def successor(self, state):
+        """За дадена состојба, врати речник од парови {акција : состојба}
+        достапни од оваа состојба. Ако има многу следбеници, употребете
+        итератор кој би ги генерирал следбениците еден по еден, наместо да
+        ги генерирате сите одеднаш.
+
+        :param state: дадена состојба
+        :return:  речник од парови {акција : состојба} достапни од оваа
+                  состојба
+        :rtype: dict
+        """
+        successors = dict()
+
+        knight_x = state[0]
+        knight_y = state[1]
+
+        bishop_x = state[2]
+        bishop_y = state[3]
+
+        stars_positions = state[4]
+
+        new_x, new_y = k1(knight_x, knight_y, bishop_x, bishop_y)
+        if [knight_x, knight_y] != [new_x, new_y]:
+            successors['K1'] = (new_x, new_y, bishop_x, bishop_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = k2(knight_x, knight_y, bishop_x, bishop_y)
+        if [knight_x, knight_y] != [new_x, new_y]:
+            successors['K2'] = (new_x, new_y, bishop_x, bishop_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = k3(knight_x, knight_y, bishop_x, bishop_y)
+        if [knight_x, knight_y] != [new_x, new_y]:
+            successors['K3'] = (new_x, new_y, bishop_x, bishop_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = k4(knight_x, knight_y, bishop_x, bishop_y)
+        if [knight_x, knight_y] != [new_x, new_y]:
+            successors['K4'] = (new_x, new_y, bishop_x, bishop_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = k5(knight_x, knight_y, bishop_x, bishop_y)
+        if [knight_x, knight_y] != [new_x, new_y]:
+            successors['K5'] = (new_x, new_y, bishop_x, bishop_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = k6(knight_x, knight_y, bishop_x, bishop_y)
+        if [knight_x, knight_y] != [new_x, new_y]:
+            successors['K6'] = (new_x, new_y, bishop_x, bishop_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = k7(knight_x, knight_y, bishop_x, bishop_y)
+        if [knight_x, knight_y] != [new_x, new_y]:
+            successors['K7'] = (new_x, new_y, bishop_x, bishop_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = k8(knight_x, knight_y, bishop_x, bishop_y)
+        if [knight_x, knight_y] != [new_x, new_y]:
+            successors['K8'] = (new_x, new_y, bishop_x, bishop_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = b1(bishop_x, bishop_y, knight_x, knight_y)
+        if [bishop_x, bishop_y] != [new_x, new_y]:
+            successors['B1'] = (knight_x, knight_y, new_x, new_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = b2(bishop_x, bishop_y, knight_x, knight_y)
+        if [bishop_x, bishop_y] != [new_x, new_y]:
+            successors['B2'] = (knight_x, knight_y, new_x, new_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = b3(bishop_x, bishop_y, knight_x, knight_y)
+        if [bishop_x, bishop_y] != [new_x, new_y]:
+            successors['B3'] = (knight_x, knight_y, new_x, new_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+        new_x, new_y = b4(bishop_x, bishop_y, knight_x, knight_y)
+        if [bishop_x, bishop_y] != [new_x, new_y]:
+            successors['B4'] = (knight_x, knight_y, new_x, new_y,
+                                tuple([s for s in stars_positions if s[0] != new_x or s[1] != new_y]))
+
+
+        return successors
+
+    def actions(self, state):
+        """За дадена состојба state, врати листа од сите акции што може да
+        се применат над таа состојба
+
+        :param state: дадена состојба
+        :return: листа на акции
+        :rtype: list
+        """
+        return self.successor(state).keys()
+
+    def result(self, state, action):
+        """За дадена состојба state и акција action, врати ја состојбата
+        што се добива со примена на акцијата над состојбата
+
+        :param state: дадена состојба
+        :param action: дадена акција
+        :return: резултантна состојба
+        """
+        return self.successor(state)[action]
+
+    def goal_test(self, state):
+        """Врати True ако state е целна состојба. Даденава имплементација
+        на методот директно ја споредува state со self.goal, како што е
+        специфицирана во конструкторот. Имплементирајте го овој метод ако
+        проверката со една целна состојба self.goal не е доволна.
+
+        :param state: дадена состојба
+        :return: дали дадената состојба е целна состојба
+        :rtype: bool
+        """
+        return len(state[-1]) == 0
+
+
+if __name__ == '__main__':
+    knight = [2, 5]
+    bishop = [5, 1]
+    stars_pos = ((1, 1), (4, 3), (6, 6))
+
+    stars = Stars((knight[0], knight[1], bishop[0], bishop[1], stars_pos))
+
+    result = breadth_first_graph_search(stars)
+    print(result.solution())
+
